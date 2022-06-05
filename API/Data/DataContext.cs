f4bd49c3219c230527f37a53aa403672e983fa41
Julia <julia.myshchenko@gmail.com>
2022-06-05 19:53:56 +0300

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace API.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext([NotNullAttribute] DbContextOptions options): base (options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
